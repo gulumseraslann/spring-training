dcce4e599d88d8a4e51fb38f0f6d0603a60b07c2
mustafa.yumurtaci <mustafa.yumurtaci@trendyol.com>
2023-09-14 17:39:15 +0300

    fix: remove redundant todos

--- src/test/java/com/trendyol/bootcamp/spring/solution/ch03/RewardNetworkTests.java
+++ src/test/java/com/trendyol/bootcamp/spring/solution/ch03/RewardNetworkTests.java
@@ -14,20 +14,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
- * TODO-09: Make sure the setUp() method in the RewardNetworkTests class is annotated with @BeforeEach.
+ * TODO-09: Start by creating an ApplicationContext.
  * - In the setUp() method, create an application context using
  *   this configuration class - use run(..) static method of
  *   the SpringApplication class
  * - Then get the 'rewardNetwork' bean from the application context
  *   and assign it to a private field for use later.
- *
- * TODO-10: We can test the setup by running an empty test.
- * - Run the test testRewardForDining. If your setup() is working, you get a green bar.
- *
- * TODO-11: Finally run a real test.
- * - Copy the unit test (the @Test method) from
- *   RewardNetworkImplTests#testRewardForDining() - we are testing
- *   the same code, but using a different setup.
+ * <p>
+ * TODO-10: Finally run the test with complete configuration.
  * - Run the test - it should pass if you have configured everything
  *   correctly. Congratulations, you are done.
  * - If your test fails - did you miss the import in TODO-7 above?
