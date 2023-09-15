9141efb4ad8a21e54ea14b0bcf2be3b95bbd9e51
gulumser.aslan <gulumseraslan@hotmail.com>
2023-09-15 21:48:00 +0300

    removed ch08

--- src/main/java/com/trendyol/bootcamp/spring/ch02/greeting08/GreetingProvider.java
+++
@@ -1,6 +0,0 @@
-package com.trendyol.bootcamp.spring.ch02.greeting08;
-
-public interface GreetingProvider {
-
-    String getGreeting();
-}
--- src/main/java/com/trendyol/bootcamp/spring/ch02/greeting08/TurkishGreetingProvider.java
+++
@@ -1,8 +0,0 @@
-package com.trendyol.bootcamp.spring.ch02.greeting08;
-
-public class TurkishGreetingProvider implements GreetingProvider{
-	
-	public String getGreeting() {
-		return "Selam :)";
-	}
-}
