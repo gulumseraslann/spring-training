377ed9024a0cc40137a78a19f74531a6b8203803
Talha Eroglu <talhaerogluu35@gmail.com>
2023-09-19 01:21:43 +0300

    Implemented all TODOs for AOP lab.
    * Test changes also included.

--- src/main/java/com/trendyol/bootcamp/spring/ch05/config/AspectsConfig.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch05/config/AspectsConfig.java
@@ -3,7 +3,9 @@ package com.trendyol.bootcamp.spring.ch05.config;
 import com.trendyol.bootcamp.spring.ch05.monitor.jamon.JamonMonitorFactory;
 import com.trendyol.bootcamp.spring.ch05.monitor.MonitorFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 // TODO-04: Update Aspect related configuration
 // - Add a class-level annotation to scan for components
@@ -14,6 +16,8 @@ import org.springframework.context.annotation.Configuration;
 //    application since it will be automatically added through
 //    auto configuration.)
 @Configuration
+@ComponentScan("com.trendyol.bootcamp.spring.ch05.aspect")
+@EnableAspectJAutoProxy
 public class AspectsConfig {
 
 	@Bean
