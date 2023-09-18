371101d0817c714aca2c382fd1fc7002dd3e82c4
Mert Şen <63496927+mertsenn1@users.noreply.github.com>
2023-09-18 13:29:08 +0300

    aop tasks completed

--- src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/DBExceptionHandlingAspect.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/DBExceptionHandlingAspect.java
@@ -1,12 +1,15 @@
 package com.trendyol.bootcamp.spring.ch05.aspect;
 
 import com.trendyol.bootcamp.spring.ch05.exception.RewardDataAccessException;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 
-@Aspect	
+@Aspect
+@Component
 public class DBExceptionHandlingAspect {
 	
 	public static final String EMAIL_FAILURE_MSG = "Failed sending an email to Mister Smith : ";
@@ -19,7 +22,7 @@ public class DBExceptionHandlingAspect {
 	//  - Configure this advice method to enable logging of
 	//	  exceptions thrown by Repository class methods.
 	//	- Select the advice type that seems most appropriate.
-	
+	@AfterThrowing(value = "execution(* *..*Repository.*(..))", throwing = "e")
 	public void implExceptionHandling(RewardDataAccessException e) {
 		// Log a failure warning
 		logger.warn(EMAIL_FAILURE_MSG + e + "\n");
