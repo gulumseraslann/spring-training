d6930057c69008c17919b2cf364506962d04bc9c
Evren Ispiroglu <evrenn.ispiroglu@gmail.com>
2023-09-20 21:38:17 +0300

    Implementation of Reward For Dining. Todo 6-9

--- src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
@@ -5,6 +5,7 @@ import com.trendyol.bootcamp.spring.ch05.monitor.MonitorFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
@@ -53,7 +54,7 @@ public class LoggingAspect {
     // - Mark this method as an around advice.
 	// - Write a pointcut expression to match on all update* methods
 	//	 on all Repository classes.
-
+	@Around("execution(* *..*Repository.update*(..))")
 	public Object monitor(ProceedingJoinPoint repositoryMethod) throws Throwable {
 		String name = createJoinPointTraceName(repositoryMethod);
 		Monitor monitor = monitorFactory.start(name);
@@ -64,7 +65,7 @@ public class LoggingAspect {
 			//  - Be sure to return the target method's return value to the caller
 			//    and delete the line below.
 
-			return new String("Delete this line after completing TODO-08");
+			return repositoryMethod.proceed();
 
 		} finally {
 			monitor.stop();
