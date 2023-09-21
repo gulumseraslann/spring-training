6d1d26770f09c8d69fc3d7e8e701f95d34216986
gmdeniz <88988425+gmdeniz@users.noreply.github.com>
2023-09-21 22:10:07 +0300

    Todos 1-4 done

--- src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
+++ src/main/java/com/trendyol/bootcamp/spring/ch05/aspect/LoggingAspect.java
@@ -5,8 +5,12 @@ import com.trendyol.bootcamp.spring.ch05.monitor.MonitorFactory;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.Signature;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 // 	TODO-02: Use AOP to log a message before
 //           any repository's find...() method is invoked.
@@ -15,7 +19,8 @@ import org.slf4j.LoggerFactory;
 //	- Optionally place @Autowired annotation on the constructor
 //    where `MonitorFactory` dependency is being injected.
 //    (It is optional since there is only a single constructor in the class.)
-
+@Aspect
+@Component
 public class LoggingAspect {
     public final static String BEFORE = "'Before'";
     public final static String AROUND = "'Around'";
@@ -23,7 +28,7 @@ public class LoggingAspect {
 	private Logger logger = LoggerFactory.getLogger(getClass());
 	private MonitorFactory monitorFactory;
 
-	
+	@Autowired
 	public LoggingAspect(MonitorFactory monitorFactory) {
 		super();
 		this.monitorFactory = monitorFactory;
@@ -35,6 +40,7 @@ public class LoggingAspect {
 	// - Write a pointcut expression that selects only find* methods on
 	//    our repository classes.
 
+	@Before("execution(* com.trendyol.bootcamp.spring.ch05.repository.*.find*(..))")
 	public void implLogging(JoinPoint joinPoint) {
 		// Do not modify this log message or the test will fail
 		logger.info(BEFORE + " advice implementation - " + joinPoint.getTarget().getClass() + //
