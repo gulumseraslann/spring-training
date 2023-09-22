39e9d98c6f3372da68ac3332ae51ad1264003f33
bozgenc <baranzgn@yandex.com>
2023-09-22 15:52:04 +0300

    codebase is refactored, implemented todos for aop

--- src/main/java/com/trendyol/bootcamp/spring/SpringTrainingApplication.java
+++ src/main/java/com/trendyol/bootcamp/spring/SpringTrainingApplication.java
@@ -1,6 +1,7 @@
 package com.trendyol.bootcamp.spring;
 
-public class SpringTrainingApplication {
+public class
+SpringTrainingApplication {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
